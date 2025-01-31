e05a00b7ead641a46a6c055e6eb1b657cc04ea92
gaurav0173 <gauravtiwari5942@gmail.com>
2025-01-31 21:00:53 +0530

    Delete address

--- Go-ecom/src/main/java/com/ecommerce/Go_ecom/service/AddressServiceImpl.java
+++ Go-ecom/src/main/java/com/ecommerce/Go_ecom/service/AddressServiceImpl.java
@@ -80,4 +80,18 @@ public class AddressServiceImpl implements AddressService {
 
         return  modelMapper.map(updatedAddress , AddressDTO.class);
     }
+
+    @Override
+    public String deleteAddress(Long addressId) {
+        Address addressFromDatabase = addressRepository.findById(addressId)
+                .orElseThrow(()-> new ResourceNotFoundException("Address", "addressId", addressId));
+
+        User user = addressFromDatabase.getUser();
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        userRepository.save(user);
+
+        addressRepository.delete(addressFromDatabase);
+
+        return "Address successfully deleted : with Id : " + addressId;
+    }
 }
